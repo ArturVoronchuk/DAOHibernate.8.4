be3bd2b30baf638745dc907e64140966470fabb5
ArturVoronchuk <103214160+ArturVoronchuk@users.noreply.github.com>
2024-07-14 16:33:03 +0300

    Update PersonRepository.java

--- src/main/java/daohibernate/repo/PersonRepository.java
+++ src/main/java/daohibernate/repo/PersonRepository.java
@@ -11,7 +11,6 @@ import java.util.Optional;
 @Repository
 public interface PersonRepository extends JpaRepository<Person, PersonId> {
 
-
     List<Person> findPersonByCity(String city);
 
     List<Person> findPersonByAge(int age);
