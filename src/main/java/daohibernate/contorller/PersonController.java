531b2154133197d8d6f62550f8cb104acec13be6
ArturVoronchuk <103214160+ArturVoronchuk@users.noreply.github.com>
2024-07-14 15:57:35 +0300

    Update PersonController.java

--- src/main/java/daohibernate/contorller/PersonController.java
+++ src/main/java/daohibernate/contorller/PersonController.java
@@ -18,7 +18,24 @@ public class PersonController {
     @GetMapping("/persons/by-city")
     @ResponseBody
     public List<Person> getPersonsByCity(@RequestParam("city") String city) {
-        List<Person> result = personRepository.getPersonsByCity(city);
+        List<Person> result = personRepository.findPersonByCity(city);
+        System.out.println(result);
+        return result;
+    }
+
+    @GetMapping("/persons/by-age")
+    @ResponseBody
+    public List<Person> getPersonsByAge(@RequestParam("age") int age) {
+        List<Person> result = personRepository.findPersonByAge(age);
+        System.out.println(result);
+        return result;
+    }
+
+    @GetMapping("/persons/by-name")
+    @ResponseBody
+    public Optional<Person> getPersonsByName(@RequestParam("name") String name,
+                                         @RequestParam("surname") String surname) {
+        Optional<Person> result = personRepository.findPersonByName(name,surname);
         System.out.println(result);
         return result;
     }
